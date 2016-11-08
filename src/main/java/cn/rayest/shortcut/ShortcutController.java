a3c58b033f3669905b27b47dc3b0863eeae7db00
Rayest <634178463@qq.com>
2016-11-09 00:15:22 +0800

    springmvc常用请求注解

--- src/main/java/cn/rayest/shortcut/ShortcutController.java
+++ src/main/java/cn/rayest/shortcut/ShortcutController.java
@@ -5,10 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Rayest on 2016/11/8 0008.
+ * 对应的快捷页面转向是在配置文件中以重写的方法实现的
  */
 @Controller
 public class ShortcutController {
-    @RequestMapping(value = "/shortcut")
+    @RequestMapping(value = "/shortcut/long")
     public String getShortcut(){
         return "shortcut";
     }
